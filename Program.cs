6585251357ee2fe672b678ea324e0388c94b2a47
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-07-21 22:24:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace c_opp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            zoo[] zoos = new zoo[3];
+
+            for (int i = 0;i < 3;i++)
+            {
+                Console.WriteLine("enter the name of zoo :", i + 1);
+                zoos[i].Name = Console.ReadLine();
+
+                Console.WriteLine("enter number of animals ", zoos[i].Name);
+                zoos[i].animalcount = int.Parse(Console.ReadLine());
+            }
+
+
+            int maxIndex= 0;
+            for (int i =1;i<3;i++)
+            {
+                if (zoos[i].animalcount > zoos[maxIndex].animalcount)
+                    maxIndex = i;
+            }
+
+            Console.WriteLine($"the name of the zoo with the max animals {zoos[maxIndex].Name} ");
+            Console.WriteLine($"the animal count {zoos[maxIndex].animalcount}");
+
+
+
+        }
+    }
+}
---
+++ zoo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_opp01
+{
+    internal struct zoo
+    {
+        public string Name;
+        public int animalcount;
+    }
+}
